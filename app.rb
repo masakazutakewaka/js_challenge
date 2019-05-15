27200ee97fabd7bcc224203956f739550dc0a2d3
masakazutakewaka <takewakamma@gmail.com>
2019-05-16 05:11:03 +0900

    Product

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require "sinatra/activerecord"
+require "./models/product"
 
 class App < Sinatra::Base
   configure :development do
@@ -11,6 +12,7 @@ class App < Sinatra::Base
   end
 
   post '/search' do
-    "#{params[:id]} is enterd"
+    p = Product.create!(asin: params[:id], category: 'baby', rank: 43, dimension: "32 X 33 X 55")
+    haml :product, format: :html5, locals: { product: p }
   end
 end

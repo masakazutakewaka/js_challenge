8b7c6d14e0fb0ff3feee7a94b2b5ef2eb9d29d27
masakazutakewaka <takewakamma@gmail.com>
2019-05-17 05:05:54 +0900

    Crawler

--- app.rb
+++ app.rb
@@ -1,10 +1,21 @@
 require 'sinatra/base'
 require "sinatra/activerecord"
 require "./models/product"
+require "./models/crawler"
 
 class App < Sinatra::Base
+  set :show_exceptions, :after_handler # need this to invoke error handlers
+
   configure :development do
     require 'byebug'
+  end
+
+  error Crawler::NotFoundError do
+    "The item was not found"
+  end
+
+  error Crawler::ParseError do
+    "failed to parse the page"
   end
 
   get '/' do
@@ -12,7 +23,9 @@ class App < Sinatra::Base
   end
 
   post '/search' do
-    p = Product.create!(asin: params[:id], category: 'baby', rank: 43, dimension: "32 X 33 X 55")
-    haml :product, format: :html5, locals: { product: p }
+    url = "https://www.amazon.com/dp/#{params[:id]}"
+    crawler = Crawler.new(url)
+    product = Product.create!(crawler.parsed_result)
+    haml :product, format: :html5, locals: { product: product }
   end
 end
---
+++ models/crawler.rb
@@ -0,0 +1,50 @@
+require 'nokogiri'
+require 'open-uri'
+
+class Crawler
+  class NotFoundError < StandardError; end
+  class ParseError < StandardError; end
+
+  ASIN_XPATH = "//div[@class='column col2 ']//tr[1]/td[@class='value']"
+  CATEGORY_XPATH = "//div[@id='wayfinding-breadcrumbs_container']//li[1]/span[@class='a-list-item']/a"
+  RANK_XPATH = "//tr[@id='SalesRank']/td[@class='value']"
+  DIMENSION_XPATH = "//tr[@class='size-weight'][2]/td[@class='value']"
+
+  def initialize(url)
+    @url = url
+  end
+
+  def parsed_result
+    { asin: asin, category: category, rank: rank, dimension: dimension }
+  end
+
+  private
+
+  def asin
+    parse(ASIN_XPATH).content
+  end
+
+  def category
+    parse(CATEGORY_XPATH).content.strip
+  end
+
+  def rank
+    parse(RANK_XPATH).content.match(/\#(\d+)/)[1]
+  end
+
+  def dimension
+    parse(DIMENSION_XPATH).content
+  end
+
+  def parse(xpath)
+    elements = doc.xpath(xpath)
+    raise ParseError if elements.empty?
+    elements.first
+  end
+
+  def doc
+    @doc ||= Nokogiri::HTML(open(@url, { "User-Agent" => '' })) # need User-Agent to go through FireWall
+  rescue OpenURI::HTTPError
+    raise NotFoundError
+  end
+end
